20aa3181e5e2303ffda66415d2ea9dccfc53cb50
ilai perez <ilai.prz@gmail.com>
2016-11-20 13:14:16 +0200

    mainmenu updates
    main menu animated and background changed

--- js/stickyDiv.js
+++ js/stickyDiv.js
@@ -7,8 +7,15 @@ $(document).ready(function() {
         if (scroll >= pos) {
 
             stickyDiv.css({ position: 'fixed' });
+            stickyDiv.css({ width: '480px' });
+            stickyDiv.css({ 'border-left': '8px solid' });
+            stickyDiv.css({ 'border-radius': '10px' });
         } else {
             stickyDiv.css({ position: '' });
+            stickyDiv.css({ width: '472px' });
+            stickyDiv.css({ width: '100%' });
+            stickyDiv.css({ border: '' });
+            stickyDiv.css({ 'border-radius': '0px' });
         }
     });
 });
